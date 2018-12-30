08442a5bf3c1f487b8d654f4c33e73464c6c27cc
jcheron <myaddressmail@gmail.com>
2018-12-30 17:30:13 +0100

    Adds MicroApp service

---
+++ services/BenchmarkMicroApp.php
@@ -0,0 +1,34 @@
+<?php
+namespace PhpBenchmarksPhalcon\RestApi\services;
+
+use Phalcon\Mvc\Micro;
+use Phalcon\Di\FactoryDefault;
+
+class BenchmarkMicroApp {
+	private $app;
+	private $di;
+	
+	public function __construct(){
+		$this->di = new FactoryDefault();
+		$this->app = new Micro($this->di);
+	}
+	
+	protected function addRoutes(){
+		$func=function(){
+			$this->doStuff();
+		};
+		$this->app->get("/benchmark/rest", $func);
+		for($i=0;$i<500;$i++){
+			$this->app->get('/benchmark/test-route-'.$i,$func);
+		}
+	}
+	
+	protected function doStuff(){
+		echo "Okay";
+	}
+	
+	public function handle(){
+		$this->app->handle();
+	}
+}
+
